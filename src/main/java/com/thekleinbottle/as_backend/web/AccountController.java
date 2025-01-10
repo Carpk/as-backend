5624c82e5f9bf0f7bf19179f3455f6eff2c07d8d
Invalid <zappeln@gmail.com>
2025-01-09 19:56:05 -0600

    additional endpoints for testing

--- src/main/java/com/thekleinbottle/as_backend/web/AccountController.java
+++ src/main/java/com/thekleinbottle/as_backend/web/AccountController.java
@@ -1,8 +1,23 @@
 package com.thekleinbottle.as_backend.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thekleinbottle.as_backend.domain.Account;
+import com.thekleinbottle.as_backend.domain.AccountRepository;
 
 @RestController
 public class AccountController {
+    private final AccountRepository repository;
+
+    public AccountController(AccountRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @GetMapping("/account")
+    public String getAccount() {
+        return new String("Your Account"); 
+    }  
 
 }
--- src/main/java/com/thekleinbottle/as_backend/web/AssetController.java
+++ src/main/java/com/thekleinbottle/as_backend/web/AssetController.java
@@ -1,9 +1,23 @@
 package com.thekleinbottle.as_backend.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thekleinbottle.as_backend.domain.Asset;
+import com.thekleinbottle.as_backend.domain.AssetRepository;
 
 @RestController
 public class AssetController {
+    private final AssetRepository repository;
 
+    public AssetController(AssetRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @GetMapping("/assets")
+    public Iterable<Asset> getAssets() {
+        return repository.findAll();
+    }
     
 }
