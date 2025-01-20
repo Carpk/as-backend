feb6aa5248472a4ee64592f7de4aa8a35c9f7084
Invalid <zappeln@gmail.com>
2025-01-20 12:34:56 -0600

    add CorsConfig

--- src/main/java/com/thekleinbottle/as_backend/SecurityConfig.java
+++ src/main/java/com/thekleinbottle/as_backend/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.thekleinbottle.as_backend;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +15,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.thekleinbottle.as_backend.service.UserDetailsServiceImpl;
 import com.thekleinbottle.as_backend.AuthenticationFilter;
@@ -48,15 +54,30 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable())
+        http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
         .sessionManagement(
             (sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             (authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login")
-        .permitAll().anyRequest().authenticated())
-        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(authEntryPoint));
+            .permitAll() // add role based access control here
+            .anyRequest().authenticated())
+            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(authEntryPoint));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowCredentials(false);
+        config.applyPermitDefaultValues();
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
